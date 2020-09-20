879b39a0c034844340fb8c44a6f8161da82dfc8e
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-09-20 00:46:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.collect do |attendee, index|
+  attendees.each_with_index.collect do |attendee, index|
     "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   end
 end
