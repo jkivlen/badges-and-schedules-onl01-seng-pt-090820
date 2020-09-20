1c814f9a1dcd589c2bff2e8a4ed42e71df668588
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-09-20 00:40:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,8 @@ def badge_maker(name)
     "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(name)
-  name.ne
-
+def batch_badge_creator(attendees)
+  attendees.map do |attendee|
+    "Hello, my name is #{attendee)."
+    end
 end
